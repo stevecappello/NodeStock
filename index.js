bfebea1ef35895ca08cef1a3ba0eec9d5e2a4245
Steve Cappello <stevecappello@ymail.com>
2025-02-19 19:14:52 -0500

    before HB ver change

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+//const exphbs = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+//Set Handlebars Middleware
+//app.engine('handlebars', exphbs());
+//app.set('view engine', 'handlebars');
+
+//Set Handlebar Routes
+//app.get('/', function (req, res) {
+ //   res.render('home');
+//});
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT ,() => console.log('Server Listening on Port ' + PORT ))
+
